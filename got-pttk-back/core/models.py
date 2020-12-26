372c769da638f37f11fd0e83540bdcac1d730484
tojatos <tojatos@gmail.com>
2020-12-26 05:07:56 +0100

    Fixed pttk models and migration

--- got-pttk-back/core/models.py
+++ got-pttk-back/core/models.py
@@ -3,30 +3,29 @@
 #   * Rearrange models' order
 #   * Make sure each model has one field with primary_key=True
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 
-class Gropagorskaprzodownika(models.Model):
+class Grupagorskaprzodownika(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
     grupagorska = models.ForeignKey('Grupagorska', models.DO_NOTHING, db_column='GrupaGorska')  # Field name made lowercase.
     przodownik = models.ForeignKey('Uzytkownik', models.DO_NOTHING, db_column='Przodownik')  # Field name made lowercase.
 
     class Meta:
-        managed = False
-        db_table = 'GropaGorskaPrzodownika'
+        db_table = 'GrupaGorskaPrzodownika'
+        unique_together = ('grupagorska', 'przodownik')
 
 
 class Grupagorska(models.Model):
-    nazwa = models.CharField(db_column='Nazwa', max_length=255)  # Field name made lowercase.
+    nazwa = models.CharField(db_column='Nazwa', max_length=255, primary_key=True)  # Field name made lowercase.
 
     class Meta:
-        managed = False
         db_table = 'GrupaGorska'
 
 
 class Odznaka(models.Model):
-    nazwa = models.CharField(db_column='Nazwa', max_length=255)  # Field name made lowercase.
+    nazwa = models.CharField(db_column='Nazwa', max_length=255, primary_key=True)  # Field name made lowercase.
     wymaganaodznaka = models.ForeignKey('self', models.DO_NOTHING, db_column='WymaganaOdznaka', blank=True, null=True)  # Field name made lowercase.
     stopien = models.CharField(db_column='Stopien', max_length=255)  # Field name made lowercase.
     wymaganaliczbapunktow = models.IntegerField(db_column='WymaganaLiczbaPunktow')  # Field name made lowercase.
@@ -35,18 +34,18 @@ class Odznaka(models.Model):
     innewymagania = models.CharField(db_column='InneWymagania', max_length=255, blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
-        managed = False
         db_table = 'Odznaka'
 
 
 class Odznakaturysty(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
     turysta = models.ForeignKey('Uzytkownik', models.DO_NOTHING, db_column='Turysta')  # Field name made lowercase.
     odznaka = models.ForeignKey(Odznaka, models.DO_NOTHING, db_column='Odznaka')  # Field name made lowercase.
-    datazdobycia = models.TextField(db_column='DataZdobycia', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    datazdobycia = models.DateField(db_column='DataZdobycia', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
 
     class Meta:
-        managed = False
         db_table = 'OdznakaTurysty'
+        unique_together = ('turysta', 'odznaka')
 
 
 class Polaczenie(models.Model):
@@ -57,7 +56,6 @@ class Polaczenie(models.Model):
     grupagorska = models.ForeignKey(Grupagorska, models.DO_NOTHING, db_column='GrupaGorska')  # Field name made lowercase.
 
     class Meta:
-        managed = False
         db_table = 'Polaczenie'
 
 
@@ -69,18 +67,18 @@ class Polaczenietrasy(models.Model):
     trasa = models.ForeignKey('Trasa', models.DO_NOTHING, db_column='Trasa')  # Field name made lowercase.
 
     class Meta:
-        managed = False
         db_table = 'PolaczenieTrasy'
 
 
 class Potwierdzenieprzebyciatrasy(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
     czyprzodownikuczestniczyl = models.IntegerField(db_column='CzyPrzodownikUczestniczyl')  # Field name made lowercase.
     trasa = models.ForeignKey('Trasa', models.DO_NOTHING, db_column='Trasa')  # Field name made lowercase.
     przodownik = models.ForeignKey('Uzytkownik', models.DO_NOTHING, db_column='Przodownik')  # Field name made lowercase.
 
     class Meta:
-        managed = False
         db_table = 'PotwierdzeniePrzebyciaTrasy'
+        unique_together = ('trasa', 'przodownik')
 
 
 class Punktpolaczenia(models.Model):
@@ -90,48 +88,41 @@ class Punktpolaczenia(models.Model):
     polaczenieid = models.ForeignKey(Polaczenie, models.DO_NOTHING, db_column='PolaczenieID')  # Field name made lowercase.
 
     class Meta:
-        managed = False
         db_table = 'PunktPolaczenia'
 
 
 class Punkttrasy(models.Model):
-    nazwa = models.CharField(db_column='Nazwa', max_length=255)  # Field name made lowercase.
+    nazwa = models.CharField(db_column='Nazwa', max_length=255, primary_key=True)  # Field name made lowercase.
 
     class Meta:
-        managed = False
         db_table = 'PunktTrasy'
 
 
 class Rola(models.Model):
-    nazwa = models.CharField(db_column='Nazwa', max_length=255)  # Field name made lowercase.
+    nazwa = models.CharField(db_column='Nazwa', max_length=255, primary_key=True)  # Field name made lowercase.
 
     class Meta:
-        managed = False
         db_table = 'Rola'
 
 
 class Trasa(models.Model):
     id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    datarozpoczecia = models.TextField(db_column='DataRozpoczecia', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
-    datazakonczenia = models.TextField(db_column='DataZakonczenia', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    datarozpoczecia = models.DateField(db_column='DataRozpoczecia', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    datazakonczenia = models.DateField(db_column='DataZakonczenia', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
     nazwa = models.CharField(db_column='Nazwa', max_length=255)  # Field name made lowercase.
-    jestplanowana = models.IntegerField(db_column='JestPlanowana')  # Field name made lowercase.
-    jestodbywana = models.IntegerField(db_column='JestOdbywana')  # Field name made lowercase.
 
     class Meta:
-        managed = False
         db_table = 'Trasa'
 
 
 class Uzytkownik(models.Model):
-    login = models.CharField(db_column='Login', max_length=255)  # Field name made lowercase.
-    jestniepelnosprawny = models.IntegerField(db_column='JestNiepelnosprawny', blank=True, null=True)  # Field name made lowercase.
-    dataurodzenia = models.TextField(db_column='DataUrodzenia', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    login = models.CharField(db_column='Login', max_length=255, primary_key=True)  # Field name made lowercase.
+    jestniepelnosprawny = models.BooleanField(db_column='JestNiepelnosprawny', blank=True, null=True)  # Field name made lowercase.
+    dataurodzenia = models.DateField(db_column='DataUrodzenia', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
     imie = models.CharField(db_column='Imie', max_length=255)  # Field name made lowercase.
     nazwisko = models.CharField(db_column='Nazwisko', max_length=255)  # Field name made lowercase.
     numerlegitymacji = models.CharField(db_column='NumerLegitymacji', max_length=255, blank=True, null=True)  # Field name made lowercase.
     rola = models.ForeignKey(Rola, models.DO_NOTHING, db_column='Rola')  # Field name made lowercase.
 
     class Meta:
-        managed = False
         db_table = 'Uzytkownik'
