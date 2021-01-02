8519e378872dcee36e91f71e8783432722e8bd79
Anna <annapapros21@gmail.com>
2021-01-02 02:58:18 +0100

    add react router

---
+++ got-pttk-front/src/Router.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { BrowserRouter , Switch, Route } from 'react-router-dom';
+import { Routes } from './constant/Routes';
+import HomePage from './screens/Home';
+import ManageRoutes from './screens/ManageRoutes';
+import ManageSegments from './screens/ManageSegments';
+import PlanRoute from './screens/PlanRoute';
+
+const routes = [
+    {
+        path: Routes.HOME,
+        component: HomePage,
+    },
+    {
+        path: Routes.MANAGE_ROUTES,
+        component: ManageRoutes,
+    },
+    {
+        
+        path: Routes.MANAGE_SEGMENTS,
+        component: ManageSegments,
+    },
+    {
+        
+        path: Routes.PLAN_ROUTE,
+        component: PlanRoute,
+    },
+    {
+        
+        path: Routes.VERIFY_ROUTE,
+        component: PlanRoute,
+    }
+];
+
+export default function Router() {
+    return (
+    <BrowserRouter >
+    <Switch>
+        {routes.map((route, i) => (
+          <Route key={i} {...route} />
+        ))}
+    </Switch>
+    </BrowserRouter>
+    )
+}
---
+++ got-pttk-front/src/screens/ManageRoutes.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default function ManageRoutes()  {
+    return <Layout><p>Zarządzaj trasami</p></Layout>;
+}
