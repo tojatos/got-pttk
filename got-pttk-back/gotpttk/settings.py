85ee89828bcbfe62f9ed88a6b65c17ba4622aaf7
tojatos <tojatos@gmail.com>
2021-01-07 11:03:56 +0100

    switched from sqlite to mariadb

--- got-pttk-back/gotpttk/settings.py
+++ got-pttk-back/gotpttk/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'gotpttk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pttk',
+        'USER': 'pttk',
+        'PASSWORD': 'pttk',
     }
 }
 
