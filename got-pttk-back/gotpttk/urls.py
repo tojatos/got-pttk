e5b3de4bc44dcc130fbd73e9579b2840df0c3fd6
tojatos <tojatos@gmail.com>
2021-01-09 13:35:06 +0100

    Added role route

--- got-pttk-back/gotpttk/urls.py
+++ got-pttk-back/gotpttk/urls.py
@@ -22,7 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/role/', views.RoleView.as_view(), name='role'),
 
-    path('hello/', views.HelloView.as_view(), name='hello'),
-    path('przewodnik/', views.PrzewodnikView.as_view(), name='przewodnik'),
+    # path('hello/', views.HelloView.as_view(), name='hello'),
+    # path('przewodnik/', views.PrzewodnikView.as_view(), name='przewodnik'),
 ]
