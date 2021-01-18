a161376790473bc41aa1dae9953260f2fc207545
tojatos <tojatos@gmail.com>
2021-01-18 21:08:38 +0100

    Some tests added

--- got-pttk-back/core/tests.py
+++ got-pttk-back/core/tests.py
@@ -1,3 +1,40 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+class ApiClientTestCase(TestCase):
+    fixtures = ['data.yaml']
+
+    def setUp(self):
+        self.client = APIClient()
+
+
+class UserApiClientTestCase(ApiClientTestCase):
+    def setUp(self):
+        super(UserApiClientTestCase, self).setUp()
+
+        tokens = self.client.post('/api/token/', {'username': 'user', 'password': 'user'}).data
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + tokens['access'])
+
+
+class SegmentsApiTest(ApiClientTestCase):
+    def test_retrieve_segment_list(self):
+        res = self.client.get('/api/segments/')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 967)
+
+
+class UnauthorizedUserSegmentsApiTest(ApiClientTestCase):
+    def test_unauthorized(self):
+        res = self.client.get('/api/user_segments/')
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class UserSegmentsApiTest(UserApiClientTestCase):
+    def test_retrieve_user_segments(self):
+        res = self.client.get('/api/user_segments/')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
